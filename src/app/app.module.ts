74ef348e76f152bfe5d042173bd0d9fe6ea8e1a3
JhonatanD09 <joranada@gmail.com>
2022-07-23 18:59:44 -0500

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SocialNetworksComponent } from './components/social-networks/social-networks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialNetworksComponent,
   ],
   imports: [
     BrowserModule
